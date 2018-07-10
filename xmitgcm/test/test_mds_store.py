e8ae40b8761ef77044a233af5f85158aa523c266
Raphael Dussin <raphael.dussin@gmail.com>
2018-07-10 15:27:27 -0400

    try to fool code cov

--- xmitgcm/test/test_mds_store.py
+++ xmitgcm/test/test_mds_store.py
@@ -264,11 +264,12 @@ def test_read_raw_data(tmpdir,dtype):
             _ = read_raw_data(fname, dtype, shape_subset,
                               offset=4, partial_read=False, use_mmap=True)
             # offset is too big
-            _ = read_raw_data(fname, dtype, shape, offset=(
-                shape[0]*shape[1]*shape[2]*dtype.itemsize), partial_read=True)
-            _ = read_raw_data(fname, dtype, shape, offset=(
-                shape[0]*shape[1]*shape[2]*dtype.itemsize), partial_read=True,
-                use_mmap=True)
+           for k in np.arange(50):
+                _ = read_raw_data(fname, dtype, shape, offset=(
+                    shape[0]*shape[1]*shape[2]*dtype.itemsize), partial_read=True)
+                _ = read_raw_data(fname, dtype, shape, offset=(
+                    shape[0]*shape[1]*shape[2]*dtype.itemsize), partial_read=True,
+                    use_mmap=True)
 
 # a meta test
 def test_file_hiding(all_mds_datadirs):
