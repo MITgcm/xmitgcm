22e96cd402bd62aa74fa6ac2cd4339f301379efe
stickler-ci <support@stickler-ci.com>
2018-09-11 12:07:48 +0000

    Fixing style errors.

--- xmitgcm/test/test_utils.py
+++ xmitgcm/test/test_utils.py
@@ -63,29 +63,29 @@ def hide_file(origdir, *basenames):
 _experiments = {
     'global_oce_latlon': {'geometry': 'sphericalpolar',
                           'shape': (15, 40, 90), 'test_iternum': 39600,
-                          'expected_values': {'XC': ((0,0), 2)},
+                          'expected_values': {'XC': ((0, 0), 2)},
                           'dtype': np.dtype('f4'),
                           'layers': {'1RHO': 31},
                           'diagnostics': ('DiagGAD-T',
-                              ['TOTTTEND', 'ADVr_TH', 'ADVx_TH', 'ADVy_TH',
-                               'DFrE_TH', 'DFxE_TH', 'DFyE_TH', 'DFrI_TH',
-                               'UTHMASS', 'VTHMASS', 'WTHMASS'])},
+                                          ['TOTTTEND', 'ADVr_TH', 'ADVx_TH', 'ADVy_TH',
+                                           'DFrE_TH', 'DFxE_TH', 'DFyE_TH', 'DFrI_TH',
+                                           'UTHMASS', 'VTHMASS', 'WTHMASS'])},
     'barotropic_gyre': {'geometry': 'cartesian',
                         'shape': (1, 60, 60), 'test_iternum': 10,
                         'dtype': np.dtype('f4'),
-                          'expected_values': {'XC': ((0,0), 10000.0)},
+                        'expected_values': {'XC': ((0, 0), 10000.0)},
                         'all_iters': [0, 10],
                         'prefixes': ['T', 'S', 'Eta', 'U', 'V', 'W']},
     'internal_wave': {'geometry': 'sphericalpolar',
                       'shape': (20, 1, 30), 'test_iternum': 100,
                       'dtype': np.dtype('f8'),
-                      'expected_values': {'XC': ((0,0), 109.01639344262296)},
+                      'expected_values': {'XC': ((0, 0), 109.01639344262296)},
                       'all_iters': [0, 100, 200],
                       'ref_date': "1990-1-1",
                       'delta_t': 60,
-                      'expected_time':[
-                        (0, np.datetime64('1990-01-01T00:00:00.000000000')),
-                        (1, np.datetime64('1990-01-01T01:40:00.000000000'))],
+                      'expected_time': [
+                          (0, np.datetime64('1990-01-01T00:00:00.000000000')),
+                          (1, np.datetime64('1990-01-01T01:40:00.000000000'))],
                       # these diagnostics won't load because not all levels
                       # were output...no idea how to overcome that bug
                       # 'diagnostics': ('diagout1', ['UVEL', 'VVEL']),
@@ -93,30 +93,30 @@ _experiments = {
     'global_oce_llc90': {'geometry': 'llc',
                          'ref_date': "1948-01-01 12:00:00",
                          'delta_t': 3600,
-                         'expected_time':[
+                         'expected_time': [
                              (0, np.datetime64('1948-01-01T12:00:00.000000000')),
                              (1, np.datetime64('1948-01-01T20:00:00.000000000'))],
                          'shape': (50, 13, 90, 90), 'test_iternum': 8,
                          'dtype': np.dtype('f4'),
-                         'expected_values': {'XC': ((2,3,5), -32.5)},
+                         'expected_values': {'XC': ((2, 3, 5), -32.5)},
                          'diagnostics': ('state_2d_set1', ['ETAN', 'SIarea',
-                            'SIheff', 'SIhsnow', 'DETADT2', 'PHIBOT',
-                            'sIceLoad', 'MXLDEPTH', 'oceSPDep', 'SIatmQnt',
-                            'SIatmFW', 'oceQnet', 'oceFWflx', 'oceTAUX',
-                            'oceTAUY', 'ADVxHEFF', 'ADVyHEFF', 'DFxEHEFF',
-                            'DFyEHEFF', 'ADVxSNOW', 'ADVySNOW', 'DFxESNOW',
-                            'DFyESNOW', 'SIuice', 'SIvice'])},
+                                                           'SIheff', 'SIhsnow', 'DETADT2', 'PHIBOT',
+                                                           'sIceLoad', 'MXLDEPTH', 'oceSPDep', 'SIatmQnt',
+                                                           'SIatmFW', 'oceQnet', 'oceFWflx', 'oceTAUX',
+                                                           'oceTAUY', 'ADVxHEFF', 'ADVyHEFF', 'DFxEHEFF',
+                                                           'DFyEHEFF', 'ADVxSNOW', 'ADVySNOW', 'DFxESNOW',
+                                                           'DFyESNOW', 'SIuice', 'SIvice'])},
     'curvilinear_leman': {'geometry': 'curvilinear',
                           'delta_t': 20,
                           'ref_date': "2013-11-12 12:00",
                           'shape': (35, 64, 340),
                           'test_iternum': 6,
                           'dtype': np.dtype('f4'),
-                          'expected_values': {'XC': ((0,0), 501919.21875)},
+                          'expected_values': {'XC': ((0, 0), 501919.21875)},
                           'all_iters': [0, 3, 6],
-                          'expected_time':[
-                            (0, np.datetime64('2013-11-12T12:00:00.000000000')),
-                            (1, np.datetime64('2013-11-12T12:02:00.000000000'))],
+                          'expected_time': [
+                              (0, np.datetime64('2013-11-12T12:00:00.000000000')),
+                              (1, np.datetime64('2013-11-12T12:02:00.000000000'))],
                           'prefixes': ['THETA']}
 }
 
@@ -160,6 +160,7 @@ def all_mds_datadirs(tmpdir_factory, request):
 def multidim_mds_datadirs(tmpdir_factory, request):
     return setup_mds_dir(tmpdir_factory, request)
 
+
 @pytest.fixture(scope='module', params=['global_oce_latlon',
                                         'global_oce_llc90'])
 def mds_datadirs_with_diagnostics(tmpdir_factory, request):
@@ -175,9 +176,11 @@ def mds_datadirs_with_refdate(tmpdir_factory, request):
 def layers_mds_datadirs(tmpdir_factory, request):
     return setup_mds_dir(tmpdir_factory, request)
 
+
 @pytest.fixture(scope='module', params=['global_oce_llc90'])
 def llc_mds_datadirs(tmpdir_factory, request):
     return setup_mds_dir(tmpdir_factory, request)
+
 
 def test_parse_meta(tmpdir):
     """Check the parsing of MITgcm .meta into python dictionary."""
@@ -198,8 +201,9 @@ def test_parse_meta(tmpdir):
     for k, v in expected.items():
         assert result[k] == v
 
+
 @pytest.mark.parametrize("dtype", [np.dtype('f8'), np.dtype('f4'), np.dtype('i4')])
-def test_read_raw_data(tmpdir,dtype):
+def test_read_raw_data(tmpdir, dtype):
     """Check our utility for reading raw data."""
 
     from xmitgcm.utils import read_raw_data
@@ -275,6 +279,8 @@ def test_read_raw_data(tmpdir,dtype):
                 use_mmap=True)
 
 # a meta test
+
+
 def test_file_hiding(all_mds_datadirs):
     dirname, _ = all_mds_datadirs
     basenames = ['XC.data', 'XC.meta']
@@ -408,7 +414,7 @@ def test_read_mds_no_meta(all_mds_datadirs):
     dtype = expected['dtype']
 
     shape_2d = (ny, nx)
-    shape_3d = shape_2d if nz==1 else (nz,) + shape_2d
+    shape_3d = shape_2d if nz == 1 else (nz,) + shape_2d
 
     prefixes = {'XC': shape_2d, 'hFacC': shape_3d}
 
@@ -436,7 +442,7 @@ def test_read_mds_no_meta(all_mds_datadirs):
 @pytest.mark.parametrize("method", ["smallchunks", "bigchunks"])
 @pytest.mark.parametrize("memmap", [True, False])
 def test_read_raw_data_llc(llc_mds_datadirs, method, memmap):
-    if memmap and method=='smallchunks':
+    if memmap and method == 'smallchunks':
         pytest.skip("Using `method='smallchunks` with `memmap=True` "
                     "opens too many files.")
 
@@ -967,8 +973,7 @@ def test_parse_diagnostics(all_mds_datadirs):
                             'long_name': 'total heat flux (match heat-content '
                             'variations), >0 increases theta',
                             'standard_name': 'TFLUX'}}
-     }
+    }
 
     for key, val in expected_diags.items():
         assert ad[key] == val
-
