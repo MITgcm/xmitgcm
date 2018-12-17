0c02ad7da85342aabb558557720f423c4fa4e2f9
stickler-ci <support@stickler-ci.com>
2018-12-17 20:00:12 +0000

    Fixing style errors.

--- xmitgcm/utils.py
+++ xmitgcm/utils.py
@@ -431,7 +431,7 @@ def parse_available_diagnostics(fname, layers={}):
                         # dimname = ('layer_' + layer_name + '_' +
                         #            suffix if suffix
                         dimname = (('l' + layer_name[0] + '_' +
-                                   suffix[0]) if suffix else '_UNKNOWN_')
+                                    suffix[0]) if suffix else '_UNKNOWN_')
                         zcoord = [dimname]
                     else:
                         zcoord = ['_UNKNOWN_']
