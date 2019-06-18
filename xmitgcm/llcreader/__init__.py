278f2a448988a533711d07b5d2688ac328bb1a6f
Ryan Abernathey <ryan.abernathey@gmail.com>
2019-06-18 15:10:43 -0400

    modify imports

--- xmitgcm/llcreader/__init__.py
+++ xmitgcm/llcreader/__init__.py
@@ -1,4 +1,5 @@
 from packaging import version
+
 try:
     import fsspec
     assert version.parse(fsspec.__version__) >= version.parse("0.2.1")
@@ -8,6 +9,17 @@ except (ImportError, AssertionError): # pramga: no cover
                       'To install it, run `pip install fsspec`. See '
                       'https://filesystem-spec.readthedocs.io/en/latest/ '
                       'for more info.')
+
+try:
+    import zarr
+    assert version.parse(zarr.__version__) >= version.parse("2.3.1")
+except (ImportError, AssertionError): # pramga: no cover
+    raise ImportError('The llcreader module requires zarr version 2.3.1 or '
+                      'or greater to be installed. '
+                      'To install it, run `pip install zarr`. See '
+                      'https://zarr.readthedocs.io/en/stable/index.html#installation '
+                      'for more info.')
+
 
 from .known_models import *
 from .stores import *
