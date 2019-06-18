11e42c593eafb0df01b309df4d2ed79e2030a67d
Ryan Abernathey <ryan.abernathey@gmail.com>
2019-06-18 14:08:31 -0400

    debugging tests

--- xmitgcm/test/test_llcreader.py
+++ xmitgcm/test/test_llcreader.py
@@ -1,6 +1,7 @@
 import pytest
 
-llcreader = pytest.importorskip("xmitgcm.llcreader")
+#llcreader = pytest.importorskip("xmitgcm.llcreader")
+import xmitgcm.llcreader as llcreader
 from .test_xmitgcm_common import llc_mds_datadirs
 
 EXPECTED_VARS = ['Eta', 'KPPhbl', 'oceFWflx', 'oceQnet', 'oceQsw', 'oceSflux',
