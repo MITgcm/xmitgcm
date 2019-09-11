31cbe0856de7ff93f6d5a0b003ac47102c354aac
Ryan Abernathey <ryan.abernathey@gmail.com>
2019-09-10 23:35:47 -0400

    Fix fsspec keyword arg bug (#170)
    * wip
    
    * increased fsspec dependency to 0.4.4 and simplified code
    
    * remove looseversion

--- xmitgcm/llcreader/__init__.py
+++ xmitgcm/llcreader/__init__.py
@@ -2,9 +2,9 @@ from packaging import version
 
 try:
     import fsspec
-    assert version.parse(fsspec.__version__) >= version.parse("0.2.1")
+    assert version.parse(fsspec.__version__) >= version.parse("0.4.4")
 except (ImportError, AssertionError): # pramga: no cover
-    raise ImportError('The llcreader module requires fsspec version 0.2.1 or '
+    raise ImportError('The llcreader module requires fsspec version 0.4.4 or '
                       'or greater to be installed. '
                       'To install it, run `pip install fsspec`. See '
                       'https://filesystem-spec.readthedocs.io/en/latest/ '
