40efeb2f88f4e9af08e619365cabbc4c4f3f9967
Ryan Abernathey <ryan.abernathey@gmail.com>
2019-10-25 11:40:56 -0400

    changed cache mechanism

--- xmitgcm/file_utils.py
+++ xmitgcm/file_utils.py
@@ -1,27 +1,37 @@
-from cachetools import cached
+import cachetools.func
 import os
 import fnmatch
 
-@cached(cache={})
+cache_maxsize = 100
+cache_ttl = 600 # tem minutes
+
+@cachetools.func.ttl_cache(maxsize=cache_maxsize, ttl=cache_ttl)
 def listdir(path):
     return os.listdir(path)
 
-@cached(cache={})
+@cachetools.func.ttl_cache(maxsize=cache_maxsize, ttl=cache_ttl)
 def listdir_startswith(path, pattern):
     files = listdir(path)
     return [f for f in files if f.startswith(pattern)]
 
-@cached(cache={})
+@cachetools.func.ttl_cache(maxsize=cache_maxsize, ttl=cache_ttl)
 def listdir_endswith(path, pattern):
     files = listdir(path)
     return [f for f in files if f.endswith(pattern)]
 
-@cached(cache={})
+@cachetools.func.ttl_cache(maxsize=cache_maxsize, ttl=cache_ttl)
 def listdir_startsandendswith(path, start, end):
     files = listdir(path)
     return [f for f in files if f.endswith(end) and f.startswith(start)]
 
-@cached(cache={})
+@cachetools.func.ttl_cache(maxsize=cache_maxsize, ttl=cache_ttl)
 def listdir_fnmatch(path, pattern):
     files = listdir(path)
     return [f for f in files if fnmatch.fnmatch(f, pattern)]
+
+def clear_cache():
+    listdir.cache_clear()
+    listdir_startswith.cache_clear()
+    listdir_endswith.cache_clear()
+    listdir_startsandendswith.cache_clear()
+    listdir_fnmatch.cache_clear()
