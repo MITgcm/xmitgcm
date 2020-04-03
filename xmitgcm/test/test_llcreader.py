8229c6a96365956b5668a53f4a2680d44cf0dfcb
Timothy Smith <timsmith204@utexas.edu>
2020-04-02 21:26:27 -0600

    try Zl...

--- xmitgcm/test/test_llcreader.py
+++ xmitgcm/test/test_llcreader.py
@@ -11,11 +11,11 @@ EXPECTED_VARS = ['Eta', 'KPPhbl', 'oceFWflx', 'oceQnet', 'oceQsw', 'oceSflux',
 EXPECTED_COORDS = {2160: ['CS','SN','Depth',
                           'drC','drF','dxC','dxG','dyC','dyG',
                           'hFacC','hFacS','hFacW','PHrefC','PHrefF','rA','rAs','rAw',
-                          'Z','Zp1','rLowC','rLowS','rLowW','rSurfC',
+                          'Z','Zl','rLowC','rLowS','rLowW','rSurfC',
                           'rSurfS','rSurfW','XC','YC'],
                    4320: ['drC','drF','dxC','dxG','dyC','dyG',
                           'hFacC','hFacS','hFacW','PHrefC','PHrefF',
-                          'rA','rAs','rAw','Z','Zp1','XC','YC']}
+                          'rA','rAs','rAw','Z','Zl','XC','YC']}
 
 ########### Generic llcreader tests on local data ##############################
 
