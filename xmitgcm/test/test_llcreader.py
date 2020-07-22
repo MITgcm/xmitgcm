ac06733b32458d5b45940c31ea80383dbb162389
Miguel Jimenez-Urias <mjimen17@jh.edu>
2020-07-22 13:18:32 -0400

    updated test to take into account arctic crown - correct size in j-direction

--- xmitgcm/test/test_llcreader.py
+++ xmitgcm/test/test_llcreader.py
@@ -6,10 +6,11 @@ llcreader = pytest.importorskip("xmitgcm.llcreader")
 from .test_xmitgcm_common import llc_mds_datadirs
 
 EXPECTED_VARS = ['Eta', 'KPPhbl', 'oceFWflx', 'oceQnet', 'oceQsw', 'oceSflux',
-            'oceTAUX', 'oceTAUY', 'PhiBot', 'Salt', 'SIarea', 'SIheff',
-            'SIhsalt', 'SIhsnow', 'SIuice', 'SIvice', 'Theta', 'U', 'V', 'W']
+                 'oceTAUX', 'oceTAUY', 'PhiBot', 'Salt', 'SIarea', 'SIheff',
+                 'SIhsalt', 'SIhsnow', 'SIuice', 'SIvice', 'Theta', 'U', 'V',
+                 'W']
 
-EXPECTED_COORDS = {2160: ['CS','SN','Depth',
+EXPECTED_COORDS = {2160: ['CS', 'SN', 'Depth',
                           'drC','drF','dxC','dxF','dxG','dyC','dyF','dyG',
                           'hFacC','hFacS','hFacW','PHrefC','PHrefF','rA','rAs','rAw',
                           'Z','Zp1','Zl','Zu','rhoRef','rLowC','rLowS','rLowW',
@@ -40,8 +41,9 @@ def test_llc90_local_faces(local_llc90_store, llc90_kwargs):
     model = llcreader.LLC90Model(store)
     ds_faces = model.get_dataset(**llc90_kwargs)
     assert set(llc90_kwargs['varnames']) == set(ds_faces.data_vars)
-    assert ds_faces.dims == {'face': 13, 'i': 90, 'i_g': 90, 'j': 90, 'j_g': 90,
-                             'k': 50, 'k_u': 50, 'k_l': 50, 'k_p1': 51, 'time': 2}
+    assert ds_faces.dims == {'face': 13, 'i': 90, 'i_g': 90, 'j': 90,
+                             'j_g': 90, 'k': 50, 'k_u': 50, 'k_l': 50,
+                             'k_p1': 51, 'time': 2}
 
 def test_llc90_local_latlon(local_llc90_store, llc90_kwargs):
     store = local_llc90_store
@@ -50,13 +52,13 @@ def test_llc90_local_latlon(local_llc90_store, llc90_kwargs):
     assert set(llc90_kwargs['varnames']) == set(ds_latlon.data_vars)
     assert ds_latlon.dims == {'i': 360, 'time': 2, 'k_p1': 51, 'face': 13,
                               'i_g': 360, 'k_u': 50, 'k': 50, 'k_l': 50,
-                              'j_g': 270, 'j': 270}
+                              'j_g': 315, 'j': 315}
 
 @pytest.mark.parametrize('rettype', ['faces', 'latlon'])
 @pytest.mark.parametrize('k_levels, kp1_levels',
-        [(None,None),
+        [(None, None),
          ([0, 2, 7, 9, 10, 20],
-          [0,1,2,3,7,8,9,10,11,20,21])])
+          [0, 1, 2, 3, 7, 8, 9, 10, 11, 20, 21])])
 @pytest.mark.parametrize('k_chunksize', [1, 2])
 def test_llc90_local_faces_load(local_llc90_store, llc90_kwargs, rettype, k_levels,
                                 kp1_levels, k_chunksize):
@@ -74,7 +76,7 @@ def test_llc90_local_faces_load(local_llc90_store, llc90_kwargs, rettype, k_leve
 
     ds.load()
 
-########### ECCO Portal Tests ##################################################
+########### ECCO Portal Tests##################################################
 
 @pytest.fixture(scope='module', params=[2160, 4320])
 def ecco_portal_model(request):
@@ -113,9 +115,9 @@ def test_ecco_portal_latlon(ecco_portal_model):
     iter_stop = ecco_portal_model.iter_start + 2 * ecco_portal_model.iter_step + 1
     ds_ll = ecco_portal_model.get_dataset(iter_stop=iter_stop, type='latlon')
     nx = ecco_portal_model.nx
-    assert ds_ll.dims == {'i': 4*nx, 'k_u': 90, 'k_l': 90, 'time': 3,
-                             'k': 90, 'j_g': 3*nx, 'i_g': 4*nx, 'k_p1': 91,
-                             'j': 3*nx, 'face': 13}
+    assert ds_ll.dims == {'i': 4 * nx, 'k_u': 90, 'k_l': 90, 'time': 3,
+                          'k': 90, 'j_g': int(3.5 * nx), 'i_g': 4 * nx,
+                          'k_p1': 91, 'j': int(3.5 * nx), 'face': 13}
     assert set(EXPECTED_VARS) == set(ds_ll.data_vars)
     assert set(EXPECTED_COORDS[nx]).issubset(set(ds_ll.coords))
 
