5935809a68f4f514f378dc4c85d496385ad3047d
Gui Castelao <guilherme@castelao.net>
2023-09-03 07:10:05 -0700

    Identifying requirements for VarZ
    Still using a hardcoded case to understand the requirements for VarZ.

--- xmitgcm/mds_tilez.py
+++ xmitgcm/mds_tilez.py
@@ -10,14 +10,27 @@ class VarZ():
         self.varname = varname
 
     def __getitem__(self, key):
-        print(f"getitem: {key}")
-        if key == ".zattrs":
-            # How to guess this?
-            return json.dumps({
-                # "_ARRAY_DIMENSIONS": ["lon", "lat", "depth"]
-                "_ARRAY_DIMENSIONS": ["lat"]
-            })
-        elif key == ".zarray":
+        print(f"VarZ.getitem: {key}")
+        if key == "lat/.zattrs":
+            return self._zattrs()
+        elif key == "lat/.zarray":
+            return self._zarray()
+        elif key == "lat/0":
+            return [ f"{self.path}/lat/0", 0, 24]
+            # np.array([11.,12.,13.]).tobytes()
+
+    def __iter__(self):
+        print(f"VarZ.__iter__()")
+        yield from ['lat/.zattrs', 'lat/.zarray', 'lat/0']
+
+    def _zattrs(self):
+        # How to guess this?
+        return json.dumps({
+            # "_ARRAY_DIMENSIONS": ["lon", "lat", "depth"]
+            "_ARRAY_DIMENSIONS": ["lat"]
+        })
+
+    def _zarray(self):
             return json.dumps({
                 # "chunks": [ 20, 30, 2 ],
                 "chunks": [ 3 ],
@@ -31,8 +44,21 @@ class VarZ():
                 "shape": [ 3 ],
                 "zarr_format": 2 # fixed
             })
-        elif key == "0":
-            lat = np.array([10.,11.,12.]).tobytes()
-            return lat
 
-        return super().__getitem__(key)
+    def items(self):
+        print(f"VarZ.items()")
+        yield from [
+            # ('lat/.zattrs', ['/Users/castelao/work/projects/others/MIT_tiles/data/example/lat/.zattrs', 0, 50]),
+            ('lat/.zattrs', self._zattrs()),
+            ('lat/.zarray', self._zarray()),
+            ('lat/0', [f"{self.path}/lat/0", 0, 24])
+        ]
+
+    def values(self):
+        print(f"VarZ.values()")
+        yield from [
+            self._zattrs(),
+            self._zarray(),
+            [f"{self.path}/lat/0", 0, 24]
+        ]
+
