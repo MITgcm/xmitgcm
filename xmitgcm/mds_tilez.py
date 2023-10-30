e276db1dcee2fb3ab8f71186a0171e398ada20a1
Gui Castelao <guilherme@castelao.net>
2023-10-29 22:54:13 -0600

    Trying the concept of VarZ()
    Chunk() is an auxiliary class to organize and collect information from
    meta, while VarZ keep track of all chunks for one variable.

--- xmitgcm/mds_tilez.py
+++ xmitgcm/mds_tilez.py
@@ -87,3 +87,95 @@ class Chunk():
             filename=filename.replace(".meta", ".data"),
             metadata=metadata)
         return chunk
+
+
+@dataclass()
+class VarZ():
+    varname: str
+    data: dict
+    chunks: tuple
+    dtype: str
+    fill_value: float
+    shape: tuple
+
+    def __getitem__(self, key):
+        print(f"VarZ.getitem: {key}")
+
+        assert key[:len(self.varname)+1] == f"{self.varname}/"
+        k = key[len(self.varname)+1:]
+        if k == f".zattrs":
+            return self._zattrs()
+        elif k == f".zarray":
+            return self._zarray()
+        else:
+            ti, xyi = k.split(".", 1)
+            ts = sorted(v.data.keys())[int(ti)]
+            return self.data[ts][xyi]
+
+    def __iter__(self):
+        print(f"VarZ.__iter__()")
+        yield from [
+            f"{self.varname}/.zattrs",
+            f"{self.varname}/.zarray",
+        ]
+        idx_t = sorted(self.data.keys())
+        for ts in idx_t:
+            yield from (f"{self.varname}/{idx_t.index(ts)}.{i}"
+                        for i in self.data[ts])
+
+    def _zattrs(self):
+        # How to guess this?
+        if len(self.shape) == 3:
+            dims = ["time", "lon", "lat", "depth"]
+        elif len(self.shape) == 2:
+            dims = ["time", "lon", "lat"]
+
+        return json.dumps({
+            # "_ARRAY_DIMENSIONS": ["lon", "lat", "depth"]
+            "_ARRAY_DIMENSIONS": dims
+        })
+
+    def _zarray(self):
+            return json.dumps({
+                "chunks": self.chunks,
+                "compressor": None, # fixed
+                "dtype": self.dtype,
+                "fill_value": self.fill_value,
+                "filters": None, # fixed
+                "order": "C", # fixed
+                "shape": self.shape,
+                "zarr_format": 2 # fixed
+            })
+
+    def push(self, chunk):
+        assert self.varname in chunk.varnames
+        assert self.chunks == chunk.chunks
+        assert self.dtype == chunk.dtype
+        assert self.shape == chunk.shape
+        assert self.fill_value == chunk.missing_value
+
+        if chunk.time_step_number not in self.data:
+            self.data[chunk.time_step_number] = {}
+        self.data[chunk.time_step_number][chunk.index] = chunk[self.varname]
+
+    def push_from_meta(self, filename):
+        d = Chunk.from_meta(filename)
+        self.push(d)
+
+    @staticmethod
+    def from_chunk(chunk, varname=None):
+        if varname is None:
+            assert len(chunk.varnames) == 1
+            varname = chunk.varnames[0]
+
+        assert varname in chunk.varnames
+
+        v = VarZ(varname=varname, data={}, chunks=chunk.chunks,
+                 shape=chunk.shape,
+                 dtype=chunk.dtype, fill_value=chunk.missing_value)
+        v.push(chunk)
+        return v
+
+    @staticmethod
+    def from_meta(filename, varname=None):
+        return VarZ.from_chunk(Chunk.from_meta(filename))
